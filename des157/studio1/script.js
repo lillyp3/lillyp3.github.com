e0ecd1befcba4d9f09eb8a0ae0b526d25e5585d1
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-01-26 18:00:27 -0800

    error messaging

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -2,10 +2,23 @@
     "use strict";
 
     var myForm = document.querySelector("#myform");
+    var madlib = document.querySelector("#madlib");
 
     myForm.addEventListener("submit", function(event){
         event.preventDefault();
         var noun1 = document.querySelector("#noun1").value;
         var noun2 = document.querySelector("#noun2").value;
+        var adj = document.querySelector("#adj").value;
+        var verb = document.querySelector("#verb").value;
+
+        if (noun1 && noun2 && adj && verb){ //add with each now input
+            var myText = `Here are the words: ${noun1}, ${noun2}, ${adj}, and ${verb}`;
+        }
+        else{
+            var myText = "Please fill out all the boxes";
+        }
+
+        madlib.innerHTML = myText;
+        
     });
 }());
