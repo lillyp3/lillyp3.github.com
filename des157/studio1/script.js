0bd08fb093265245435f7c80a23e38259589cbf5
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-01-26 17:54:40 -0800

    add to form

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -1,4 +1,11 @@
 (function(){
     "use strict";
-    
+
+    var myForm = document.querySelector("#myform");
+
+    myForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        var noun1 = document.querySelector("#noun1").value;
+        var noun2 = document.querySelector("#noun2").value;
+    });
 }());
