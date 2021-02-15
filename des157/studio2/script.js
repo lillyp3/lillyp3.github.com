db8c91e99c0be34ad5a6bd2cbb96a27dfa1b6b73
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-02-15 01:29:33 -0800

    pictures

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,6 +1,22 @@
-(function(){
-    "use strict";
-    console.log("reading js");
+(function () {
+    'use strict';
+    console.log("reading");
 
-    
+    const FishPics = [
+        "fish1.jpg",
+        "fish2.jpg"
+    ];
+
+    let currentImage = 0;
+
+    const slide =document.getElementById("fishImages");
+
+    setInterval(function (){
+        currentImage++;
+
+        if (currentImage > FishPics.length-1){
+            currentImage = 0;
+        }
+
+        slide.src = `images/${FishPics[currentImage]}`;}, 1000);
 });
