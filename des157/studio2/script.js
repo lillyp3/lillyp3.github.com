a066cc0dce5b7607152cafa27e2f7793f13b16da
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-02-15 16:06:11 -0800

    picture

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -2,24 +2,6 @@
     'use strict';
     console.log("reading");
 
-    const container = document.getElementById("zoom");
-    const theImg = document.querySelector("#zoom img");
-    const percent = container.offsetWidth / 100;
-    let prevLoc = 0;
-
-    container.addEventListener("mousemove", reportPos);
-
-    function reportPos(event){
-        const mousePosX = Math.ceil((event.clientX - (container.getBoundingClientRect().left)) / percent);
-
-        if (prevLoc !== mousePosX) {
-            let addedPx = mousePosX * 30;
-            theImg.style.width = (800 + addedPx) + "px";
-            prevLoc = mousePosX;
-            console.log(prevLoc);
-        }
-    }
-
     
 });
 
