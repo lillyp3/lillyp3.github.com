3428cfd9eb16f00289107e4f076810323dc35b6b
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-03-03 21:37:27 -0800

    final

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -2,34 +2,11 @@
     'use strict';
     console.log("reading");
 
-    document.querySelector(".item").addEventListener("click", function(event){     //hover mouse over the fish design slideshow
+    document.querySelectorAll("#shirts img").addEventListener("click", function(event){     //hover mouse over the fish design slideshow
         event.preventDefault();
-        document.getElementById("fishOverlay").className = "showing";                   //changes class of overlay so it is visible
-    });
-
-    document.querySelector("button").addEventListener("click", function(event){     //hover mouse over the fish design slideshow
-        event.preventDefault();
-        document.getElementById("fishOverlay").className = "hidden";                   //changes class of overlay so it is visible
+        document.querySelectorAll("#shirts img").className = "showingColor";                   //changes class of overlay so it is visible
     });
 
 
-
-        /* Fish design */
-        let currentImage = 0;       //creates a variable to keep track of image on screen                            
-        const FishPics = [          //list of all photos in slideshow
-            "fish1.jpg",
-            "fish2.jpg",
-            "fish3.jpg"
-        ];
-        
-        const slide = document.getElementById("fishImages");        //variable that holds the div of where slideshow will be in
-        setInterval(function (){        //timed interval to change photo every 2 seconds
-            currentImage++;             //everytime the function is called, the currentImage adds one
-            
-            if (currentImage > FishPics.length-1){      //when the list reaches the end, the count starts over and starts with the first image again
-                currentImage = 0;
-            }
-            
-            slide.src = `images/${FishPics[currentImage]}`;}, 2000);            //changes the source to the image that corresponds to the index
 
 }());
