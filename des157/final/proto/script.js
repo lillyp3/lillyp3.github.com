633b8f4ee260346337c25770fdce4af86ab9bc6c
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-03-07 01:03:41 -0800

    final project

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -2,25 +2,75 @@
     'use strict';
     console.log("reading");
 
+    /* Item 01 */
+
+    document.getElementById("first").addEventListener("mouseover", function(event){         //click on shirt image, overlay changes from hidden to showing
+        event.preventDefault();
+        document.getElementById("fishText").className = "showing";                   
+    });
+
+    document.getElementById("first").addEventListener("mouseleave", function(event){         //click on shirt image, overlay changes from hidden to showing
+        event.preventDefault();
+        document.getElementById("fishText").className = "hidden";                   
+    });
+
     document.getElementById("first").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
         event.preventDefault();
-        document.getElementById("fishOverlay").className = "showing";                   
+        document.getElementById("video1").className = "showing";                
     });
 
-    document.getElementById("second").addEventListener("click", function(event){     
+/* Item 02 */
+    document.getElementById("second").addEventListener("mouseover", function(event){         //click on shirt image, overlay changes from hidden to showing
         event.preventDefault();
-        document.getElementById("fossilOverlay").className = "showing";                   
+        document.getElementById("fossilText").className = "showing";                   
     });
 
-    document.getElementById("third").addEventListener("click", function(event){     
+    document.getElementById("second").addEventListener("mouseleave", function(event){         //click on shirt image, overlay changes from hidden to showing
         event.preventDefault();
-        document.getElementById("candyOverlay").className = "showing";                   
+        document.getElementById("fossilText").className = "hidden";                   
     });
 
-    document.getElementById("fourth").addEventListener("click", function(event){     
+    document.getElementById("second").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
         event.preventDefault();
-        document.getElementById("popOverlay").className = "showing";                   
+        document.getElementById("video2").className = "showing";                   
     });
+
+/* Item 03 */
+document.getElementById("third").addEventListener("mouseover", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("candyText").className = "showing";                   
+});
+
+document.getElementById("third").addEventListener("mouseleave", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("candyText").className = "hidden";                   
+});
+
+document.getElementById("third").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("video3").className = "showing";                   
+});
+
+/* Item 04 */
+document.getElementById("fourth").addEventListener("mouseover", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("popText").className = "showing";                   
+});
+
+document.getElementById("fourth").addEventListener("mouseleave", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("popText").className = "hidden";                   
+});
+
+document.getElementById("fourth").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
+    event.preventDefault();
+    document.getElementById("video4").className = "showing";                   
+});
+
+
+
+
+
 
     
  /*     document.addEventListener("mousemove", reportPos);
