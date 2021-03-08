2463e65d559259caac65a9a8b4b05dcde06085c0
Lilly Paglia <lkpaglia@ucdavis.edu>
2021-03-07 22:42:12 -0800

    final project

--- des157/final/usertest/script.js
+++ des157/final/usertest/script.js
@@ -2,28 +2,28 @@
     'use strict';
     console.log("reading");
 
+/* Alert messages */
     alert("Hello, during this test you will be asked to complete four tasks");
     alert("1) Get from the opening message to the t-shirt designs.");
     alert("2) Choose a design.");
     alert("3) Find out more information about the design, and close out of that information.");
     alert("4) Choose a different design. Good Luck!");
 
-    let icon = document.querySelector("header p");
-    /* Item 01 */
+    let icon = document.querySelector("header p");      /* sets a variable for the upper right icon that changes depending on chosen design */
 
-    document.getElementById("first").addEventListener("click", function(event){         
+/* Item 01 */
+    document.getElementById("first").addEventListener("click", function(event){         /* click on the t-shirt */ 
         event.preventDefault();
-        document.getElementById("fishText").className = "showing"; 
-        document.querySelector("body").className = "fishBackground";
-        document.getElementById("fossilText").className = "hidden"; 
+        document.getElementById("fishText").className = "showing";                      /* div that holds video, title, about button, and colors is now displayed on screen */
+        document.querySelector("body").className = "fishBackground";                    /* background color changes */
+        document.getElementById("fossilText").className = "hidden";                     /* keeps other design information hidden and videos aren't playing to slow down page */        
         document.getElementById("candyText").className = "hidden";    
-        document.getElementById("popText").className = "hidden"; 
-        icon.innerHTML = '<img src="images/Fish.png">';        
+        document.getElementById("popText").className = "hidden";  
+        icon.innerHTML = '<img src="images/Fish.png">';                                 /* upper right icon changes */
     });
 
 /* Item 02 */
-
-    document.getElementById("second").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
+    document.getElementById("second").addEventListener("click", function(event){         
         event.preventDefault();
         document.getElementById("fossilText").className = "showing"; 
         document.querySelector("body").className = "fossilBackground"; 
@@ -34,10 +34,7 @@
     });
 
 /* Item 03 */
-
-
-    document.getElementById("third").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
-        event.preventDefault();
+    document.getElementById("third").addEventListener("click", function(event){         
         document.getElementById("candyText").className = "showing"; 
         document.querySelector("body").className = "candyBackground"; 
         document.getElementById("fishText").className = "hidden"; 
@@ -47,8 +44,7 @@
     });
 
 /* Item 04 */
-
-    document.getElementById("fourth").addEventListener("click", function(event){         //click on shirt image, overlay changes from hidden to showing
+    document.getElementById("fourth").addEventListener("click", function(event){         
         event.preventDefault();
         document.getElementById("popText").className = "showing"; 
         document.querySelector("body").className = "popBackground";  
@@ -59,85 +55,86 @@
     });
 
 
-/* BUTTONS /////////////////////////// */
 
-    document.querySelector("#fishInfo button").addEventListener("click", function(event){         
+/* Buttons to close out of overlays */
+    document.querySelector("#fishInfo button").addEventListener("click", function(event){              /* click on upper right "x" button */  
         event.preventDefault();
-        document.querySelector("#one").className = "hidden";
-        document.querySelector("#fishText").className = "showing"; 
+        document.querySelector("#one").className = "hidden";                                            /* overlay is no longer visible */
+        document.querySelector("#fishText").className = "showing";                                      /* video. title, and button is visible */
                     
     });
 
-    document.querySelector("#fossilInfo button").addEventListener("click", function(event){         
+    document.querySelector("#fossilInfo button").addEventListener("click", function(event){             /* fossil */
         event.preventDefault();
         document.querySelector("#two").className = "hidden";
         document.querySelector("#fossilText").className = "showing";
                         
     });
 
-    document.querySelector("#candyInfo button").addEventListener("click", function(event){         
+    document.querySelector("#candyInfo button").addEventListener("click", function(event){              /* hearts */
         event.preventDefault();
         document.querySelector("#three").className = "hidden";
         document.querySelector("#candyText").className = "showing"; 
                         
     });
 
-    document.querySelector("#popInfo button").addEventListener("click", function(event){         
+    document.querySelector("#popInfo button").addEventListener("click", function(event){                /* lollipop */
         event.preventDefault();
         document.querySelector("#four").className = "hidden";
         document.querySelector("#popText").className = "showing";
     });
+/* ////////////////////////////////////////////////////////////////////////////////////// */
 
-
-    document.querySelector("#fishButton").addEventListener("click", function(event){         
+/* Buttons to open the overlays */
+    document.querySelector("#fishButton").addEventListener("click", function(event){                    /* click on "about button" */
         event.preventDefault();
-        document.querySelector("#one").className = "showing";
-        document.querySelector("#fishText").className = "hidden";               
+        document.querySelector("#one").className = "showing";                                           /* overlay with slideshow + text is visible */
+        document.querySelector("#fishText").className = "hidden";                                       /* video and title is not visible on screen so it does not conflict with overlay */
     });
 
-    document.querySelector("#fossilButton").addEventListener("click", function(event){         
+    document.querySelector("#fossilButton").addEventListener("click", function(event){                  /* fossil */
         event.preventDefault();
         document.querySelector("#two").className = "showing";
         document.querySelector("#fossilText").className = "hidden";
                         
     });
 
-    document.querySelector("#candyButton").addEventListener("click", function(event){         
+    document.querySelector("#candyButton").addEventListener("click", function(event){                   /* hearts */
         event.preventDefault();
         document.querySelector("#three").className = "showing";
         document.querySelector("#candyText").className = "hidden";
                         
     });
 
-    document.querySelector("#popButton").addEventListener("click", function(event){         
+    document.querySelector("#popButton").addEventListener("click", function(event){                     /* lollipop */
         event.preventDefault();
         document.querySelector("#four").className = "showing";
         document.querySelector("#popText").className = "hidden";
                         
     });
+/* ////////////////////////////////////////////////////////////////////////////////////// */
 
-    /* SlideSHowssssssss */
-
-    let currentImage1 = 0;
-    const fishPics = [
+/* SlideSHowssssssss */
+    let currentImage1 = 0;          /* sets a variable for the image being displayed on screen */
+    const fishPics = [              /* creates an array of all items in the slideshow */
         "fish1.jpg",
         "fish2.jpg",
         "fish3.jpg"
     ];
 
-    const fishSlide = document.getElementById("fishImages");
+    const fishSlide = document.getElementById("fishImages");        /* sets a variable for the slideshow and links with where the div is in the html */
     setInterval(function (){
-        currentImage1++;
+        currentImage1++;                                            /* index of current image array increases */
         
-        if (currentImage1 > fishPics.length-1){
-            currentImage1 = 0;
+        if (currentImage1 > fishPics.length-1){                     /* if at the end of the list */
+            currentImage1 = 0;                                      /* start at the first image again */
         }
         
-        fishSlide.src = `images/${fishPics[currentImage1]}`;}, 2000);
+        fishSlide.src = `images/${fishPics[currentImage1]}`;}, 2000);       /* change the photo every 2 seconds */
 
 
-
-    let currentImage2 = 0;
+/* fish slideshow */
+    let currentImage2 = 0;                                          
     const fossilPics = [
         "fossil1.jpg",
         "fossil2.jpg",
@@ -155,7 +152,7 @@
         fossilSlide.src = `images/${fossilPics[currentImage2]}`;}, 2000);
 
 
-
+/* Hearts Slideshow */
     let currentImage3 = 0;
     const heartPics = [
         "Hearts1.jpg",
@@ -174,7 +171,7 @@
         heartSlide.src = `images/${heartPics[currentImage3]}`;}, 2000);
 
 
-
+/* Lollipop Slideshow */
     let currentImage4 = 0;
     const PopPics = [
         "pop1.jpg",
@@ -191,21 +188,6 @@
         }
         
         popSlide.src = `images/${PopPics[currentImage4]}`;}, 2000);
-
-/* mouse positioning */
-
-        document.addEventListener("mousemove", reportPos);
-
-    function reportPos(event){
-        const xPos = event.clientX;
-        const yPos = event.clientY;
-        let prevXLoc = 0;
-        let prevYLoc = 0;
-        console.log(`The mouse is ${xPos} pixels from the left and ${yPos} pixels from the top`);
-           
-    }
-
-
 
 
 }());
